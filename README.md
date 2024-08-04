38c6faeb0338a7ed1b0de07e7d790071a7dbe8a2
Underscoore <ajaykumar.bit.1995@gmail.com>
2024-08-04 07:04:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# before doing any thing
+split terminal vertically-> vsplit
+ 
+run:
+`
+npm i
+npm run dev < to run in dev mode
+npm run build < to build the project
+`
+added a one more line
+
+
---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>This is Quite amazing!</h1>
+  </div>
+`
+
+  document.querySelector('.classSelector').innerHTML = `
+  <div>This is a whole new experice </div>
+`
+/**Thiinit
+ *
+ *@param
+ *
+ * **/
+function test(){
+  return "this is amazing";
+}
